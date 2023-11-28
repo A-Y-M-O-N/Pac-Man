41dde9fb094f82dbae7181063682bcc6affa9ded
A-Y-M-O-N <150492244+A-Y-M-O-N@users.noreply.github.com>
2023-11-28 19:37:12 +1100

    Update PacMan.java

--- PacMan.java
+++ PacMan.java
@@ -1,5 +1,5 @@
 /**
- * MyClara
+ * pacman
  * 
  * Available functions (see Assignment document for explanations on what each function does):
  * treeFront, ghostWallFront,
@@ -10,17 +10,15 @@
  * onLeaf, removeLeaf, 
  * onMushroom, removeMushroom,
  * allLeavesEaten, 
- * isClaraDead,
- * playClaraDieSound, isClaraDieSoundStillPlaying,
+ * isPacmanDead,
+ * playPacmanDieSound, isPacmanDieSoundStillPlaying,
  * playLeafEatenSound,
  * playPacmanIntro, isPacmanIntroStillPlaying,
  * wrapAroundWorld,
  * getCurrentLevelNumber, advanceToLevel
  */
-class MyClara extends Clara
+class MyPacman extends Pacman
 {
-    // Please leave this first level here,
-    // until after you've completed \"Part 12 -
     // Making and Adding Levels\"
     public final char[][] LEVEL_1 = {
             {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
@@ -57,11 +55,11 @@ class MyClara extends Clara
     public final String  LEFT = "left";    
     public final String  RIGHT = "right";        
     
-    // Add and initialise Clara's variables here
+    // Add and initialise Pacman's variables here
     
-    public final int ClaraSpeed = 10;
+    public final int PacmanSpeed = 10;
     public final int count = 0;
-    public boolean moveClara = false;
+    public boolean movePacman = false;
     public boolean newLevel = true;
     boolean isGameOver = false;
     int levelCount = 2;
@@ -74,7 +72,7 @@ class MyClara extends Clara
      */
     public void act()
     {
-        //Make Clara do things here
+        //Make Pacman do things here
         if (newLevel)
         {
             // funtion that will play the Pacman introplayPacmanIntro();
@@ -85,31 +83,31 @@ class MyClara extends Clara
         // condition to check if into is still playing and if the game is not over
         if ( !isPacmanIntroStillPlaying() && !isGameOver )
         {
-            //condition to check if Clara is not dead then detect which key board key was pressed
-            if ( !isClaraDead() )
+            //condition to check if Pacman is not dead then detect which key board key was pressed
+            if ( !isPacmanDead() )
             {
                 whichKeyWasPressed();
             }
             
-            // condition to check if clara is moving is true and use the selected speed
-            if (moveClara)
+            // condition to check if Pacman is moving is true and use the selected speed
+            if (movePacman)
             {
-                move(ClaraSpeed);
+                move(PacmanSpeed);
             }
             
-            // animates clara's movement
-            animateClaraMovement();
+            // animates Pacman's movement
+            animatePacmanMovement();
 
-            // enables clara to move from one side of the world to the other 
+            // enables Pacman to move from one side of the world to the other 
             moveAroundWorld();
 
-            // prevents clara from colliding with trees and ghost wall
-            ClaraCantCollide();
+            // prevents Pacman from colliding with trees and ghost wall
+            PacmanCantCollide();
 
-            // Clara needs to eat all the leaves and when she does eating leaf sound plays
+            // Pacman needs to eat all the leaves and when he does eating leaf sound plays
             eatingAllLeaves();
 
-            // conditoin to check if all the leaves have been eaten then clara can move to the next level
+            // conditoin to check if all the leaves have been eaten then Pacman can move to the next level
             if ( allLeavesEaten() )
             {
                 advanceToLevel(LEVEL_2);
@@ -117,13 +115,13 @@ class MyClara extends Clara
             }
 
             
-            // Clara will eat mushrroms and gain super powers
+            // Pacman will eat mushrooms and gain super powers
             eatingMushrooms();
 
         }
     
     
-    //Give Clara functions here
+    //Give Pacman functions here
 
     }
 
@@ -132,31 +130,31 @@ class MyClara extends Clara
         if ( Keyboard.isKeyDown(UP) )
         {
             setDirection(UP);
-            moveClara = true;
+            movePacman = true;
         }
 
         else if ( Keyboard.isKeyDown(DOWN) )
         {
             setDirection(DOWN);
-            moveClara = true;
+            movePacman = true;
         }
 
         else if ( Keyboard.isKeyDown(RIGHT) )
         {
             setDirection(RIGHT); 
-            moveClara = true;
+            movePacman = true;
         }
 
         else if ( Keyboard.isKeyDown(LEFT) )
         {
             setDirection(LEFT); 
-            moveClara = true;
+            movePacman = true;
         }
     }
 
-    void animateClaraMovement()
+    void animatePacmanMovement()
     {
-        //clara will animate after every 5 frames
+        //Pacman will animate after every 5 frames
         if (animateCounter == 5)
         {
             animate();
@@ -167,23 +165,23 @@ class MyClara extends Clara
 
     void moveAroundWorld()
     {
-        //method to make clara move from one side of the world to the other
+        //method to make Pacman move from one side of the world to the other
         if ( !treeFront() && treeLeft() && treeRight() && treeAbove() && treeBelow() )  
         {
             wrapAroundWorld();
         }
     }
 
-    //Clara can't Collide with tree's and the Ghost wall
-    void ClaraCantCollide()
+    //Pacman can't Collide with tree's and the Ghost wall
+    void PacmanCantCollide()
     {
         if ( !ghostWallFront() ) 
         {
-            moveClara = false; 
+            movePacman = false; 
         }
         if ( !treeFront() )
         {
-            moveClara = true;
+            movePacman = true;
         }
     }
 
